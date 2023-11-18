cba9a737b47e51b54ebdfd8015560f541d49e25a
HWTjac0 <jakub3020.jmy@gmail.com>
2023-11-18 01:14:41 +0100

    Initialised project dynamic route, move getProjects to github utility file, might add types and stuff later. Just getting that minimum viable solution

--- src/utilities/github.ts
+++ src/utilities/github.ts
@@ -1,5 +1,29 @@
 import { Octokit } from "octokit";
+import { isProd } from "./env";
 
 export const octokit = new Octokit({
   auth: import.meta.env.GH_API_TOKEN,
 });
+
+export async function getProjects() {
+  const projects: any[] = []; // temporarily :P
+  const per_page = isProd ? 100 : 3;
+  let page = 1;
+  do {
+    const { data } = await octokit.request(
+      `GET /orgs/{org}/repos?page={page}&per_page={per_page}`,
+      {
+        org: "akai-org",
+        page: page,
+        per_page: per_page,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      },
+    );
+    projects.push(data);
+    page += 1;
+  } while (isProd && projects.length >= per_page * page)
+
+  return projects.flat();
+}
