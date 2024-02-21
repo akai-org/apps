b45b90efcc53254128c90c885de658e09a586968
HWTjac0 <jakub3020.jmy@gmail.com>
2024-02-21 18:58:21 +0100

    fix: build typescript check error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
-import { languageColors } from "./src/utilities/languageColors";
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
