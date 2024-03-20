c29ae91926433c61240ef2da5cfaa60bc32300ef
jacmill <jakub3020.jmy@gmail.com>
2024-03-20 21:40:11 +0100

    initial dark mode implementation and svg icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "selector",
 	theme: {
 		extend: {
       fontFamily: {
