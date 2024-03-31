70a3e8eae0c879b75a845ffd720340a75d61503b
HWTjac0 <jakub3020.jmy@gmail.com>
2024-03-31 15:27:58 +0200

    akai logo dark mode and further dark mode features

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
   darkMode: "selector",
-	theme: {
-		extend: {
+  theme: {
+    extend: {
       fontFamily: {
         sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
       }
     },
-	},
-	plugins: [
+  },
+  plugins: [
     require("@tailwindcss/typography"),
   ],
 }
