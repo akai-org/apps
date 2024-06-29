df0af0350412434d2ca836daf6c029497088e6f4
Filip Sauer <sauer.filip@gmail.com>
2024-06-29 14:27:12 +0200

    make title formatting more consistent

--- src/utilities/stringUtils.ts
+++ src/utilities/stringUtils.ts
@@ -1,4 +1,11 @@
+export const upperFirst = (text: string) =>
+  text.charAt(0).toUpperCase().concat(text.slice(1));
+
 export const capitalize = (text: string) =>
-  `${text.charAt(0).toUpperCase().concat(text.slice(1))}`;
+  text
+    .split(" ")
+    .map((word) => upperFirst(word))
+    .join(" ");
+
 export const placeWhitespaces = (name: string) =>
   name.replaceAll("_", " ").replaceAll("-", " ");
